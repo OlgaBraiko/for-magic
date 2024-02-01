35ca859d5ea1cf9adf5eee4afa8277a0d4fc9d30
OLGA BRAIKO <Olga.braiko456@mail.ru>
2024-02-02 00:43:29 +0300

    add wApp media footer route

--- modal.js
+++ modal.js
@@ -1,59 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.getElementById("modal").style.display = "block";
-  }, 5000);
+// document.addEventListener("DOMContentLoaded", function () {
+//   setTimeout(function () {
+//     document.getElementById("modal").style.display = "block";
+//   }, 5000);
 
-  const modal = document.getElementById("modal");
-  const closeBtn = modal.querySelector(".close");
+//   const modal = document.getElementById("modal");
+//   const closeBtn = modal.querySelector(".close");
 
-  closeBtn.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
+//   closeBtn.addEventListener("click", function () {
+//     modal.style.display = "none";
+//   });
 
-  const phoneForm = document.getElementById("phoneForm");
-  phoneForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const phoneNumber = document.getElementById("phoneNumber").value;
-    // Отправка данных на сервер или другая логика
-    console.log("Номер телефона:", phoneNumber);
-    modal.style.display = "none";
-  });
-});
+//   const phoneForm = document.getElementById("phoneForm");
+//   phoneForm.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     const phoneNumber = document.getElementById("phoneNumber").value;
+//     // Отправка данных на сервер или другая логика
+//     console.log("Номер телефона:", phoneNumber);
+//     modal.style.display = "none";
+//   });
+// });
 
-document
-  .getElementById("openModalButton2")
-  .addEventListener("click", function () {
-    document.getElementById("modal").style.display = "flex";
-  });
+// document.addEventListener("DOMContentLoaded", () => {
+//   const workLink = document.getElementById("work-link");
+//   const modal = document.getElementById("modal");
+//   const closeModal = document.getElementById("close-modal");
 
-document.getElementById("callMeButton").addEventListener("click", function () {
-  var phoneNumber = document.getElementById("phoneNumber").value;
-  // Здесь можно добавить код для отправки номера телефона на сервер или другую обработку
-  alert("Мы перезвоним вам по номеру " + phoneNumber);
-});
+//   workLink.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     modal.style.display = "block";
+//   });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const workLink = document.getElementById("work-link");
-  const modal = document.getElementById("modal");
-  const closeModal = document.getElementById("close-modal");
+//   closeModal.addEventListener("click", () => {
+//     modal.style.display = "none";
+//   });
 
-  workLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    modal.style.display = "block";
-  });
+//   modal.addEventListener("click", (event) => {
+//     if (event.target === modal) {
+//       modal.style.display = "none";
+//     }
+//   });
+//   document
+//     .getElementById("phoneNumber")
+//     .addEventListener("input", function (e) {
+//       e.target.value = e.target.value.replace(/[^0-9]/g, "");
+//     });
+// });
 
-  closeModal.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
-
-  modal.addEventListener("click", (event) => {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  });
-  document
-    .getElementById("phoneNumber")
-    .addEventListener("input", function (e) {
-      e.target.value = e.target.value.replace(/[^0-9]/g, "");
-    });
-});
+// // new modal
